9a12df5b2b0e7b6cd0d13efab5773373ed2d988f
Peng <52995448+mugpeng@users.noreply.github.com>
2026-08-28 16:32:33 +0800

    chore: deploy to www.wehuman.top

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://wehuman01.github.io',
-  base: '/wehuman-web',
+  site: 'https://www.wehuman.top',
+  base: '/',
   output: 'static',
   integrations: [sitemap()],
 });
